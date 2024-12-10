b6ebc4cdf80748292f404265ebdacfaea6569bf0
AbdilahiMJama <abdilahi.jama@mnsu.edu>
2024-12-10 12:15:00 -0600

    Created a navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,22 @@
 import React, { useState, useEffect } from "react";
+import Navbar from "./components/navbar";
 
 function App() {
-    const [message, setMessage] = useState("");
+  const [message, setMessage] = useState("");
 
-    useEffect(() => {
-        fetch("http://localhost:5000/")
-            .then((response) => response.json())
-            .then((data) => setMessage(data.message));
-    }, []);
+  useEffect(() => {
+    fetch("http://localhost:5000/")
+      .then((response) => response.json())
+      .then((data) => setMessage(data.message));
+  }, []);
 
-    return (
-        <div>
-            <h1>{message}</h1>
-        </div>
-    );
+  return (
+    <div>
+      <Navbar />
+      <h1>{message}</h1>
+      <h3>Hello is it me you're looking for</h3>
+    </div>
+  );
 }
 
 export default App;
